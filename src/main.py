8497455d1989c1b784908da6388c4ca2acbfd83d
codingshroom <zyt3xx@gmail.com>
2025-10-05 22:26:21 +0200

    pulled out BASIC_DECK from main.py into variables.py

--- src/main.py
+++ src/main.py
@@ -1,49 +1,8 @@
-# functions with clear input(arguments) + output(return statements)
-# using strengths of Classes, manipulating fields & cards directly in Player object
-# adding clear interface to send information from game mechanics to GUI
-
 import random
 from dataclasses import dataclass
 from typing import List, Dict
+from variables import BASIC_DECK
 
-BASIC_DECK = {  # separation in direct & charged allows for a flexible way to get to the values
-    "N": {  # novice spell
-        "direct": {"attack": 1, "block": 1, "draw": 0, "counter": True, "lifesteal": False, "reflection": False,
-                   "block_draw": False, "steal_draw": False},
-        "charged": {"attack": 2, "block": 2, "draw": 1, "counter": True, "lifesteal": False, "reflection": False,
-                    "block_draw": False, "steal_draw": False},
-    },
-    "A": {  # academy spell
-        "direct": {"attack": 2, "block": 2, "draw": 1, "counter": False, "lifesteal": False, "reflection": False,
-                   "block_draw": False, "steal_draw": False},
-        "charged": {"attack": 4, "block": 3, "draw": 2, "counter": False, "lifesteal": False, "reflection": False,
-                    "block_draw": False, "steal_draw": False},
-    },
-    "A*": {  # academy spell
-        "direct": {"attack": 2, "block": 2, "draw": 1, "counter": False, "lifesteal": False, "reflection": False,
-                   "block_draw": False, "steal_draw": False},
-        "charged": {"attack": 4, "block": 3, "draw": 2, "counter": False, "lifesteal": False, "reflection": False,
-                    "block_draw": False, "steal_draw": False},
-    },
-    "P": {  # professors spell
-        "direct": {"attack": 3, "block": 3, "draw": 2, "counter": False, "lifesteal": False, "reflection": False,
-                   "block_draw": False, "steal_draw": False},
-        "charged": {"attack": 6, "block": 4, "draw": 3, "counter": False, "lifesteal": False, "reflection": False,
-                    "block_draw": False, "steal_draw": False},
-    },
-    "P*": {  # professors spell
-        "direct": {"attack": 3, "block": 3, "draw": 2, "counter": False, "lifesteal": False, "reflection": False,
-                   "block_draw": False, "steal_draw": False},
-        "charged": {"attack": 6, "block": 4, "draw": 3, "counter": False, "lifesteal": False, "reflection": False,
-                    "block_draw": False, "steal_draw": False},
-    },
-    "F": {  # forbidden one
-        "direct": {"attack": 0, "block": 0, "draw": 0, "counter": False, "lifesteal": False, "reflection": False,
-                   "block_draw": True, "steal_draw": False},
-        "charged": {"attack": 4, "block": 0, "draw": 0, "counter": False, "lifesteal": True, "reflection": True,
-                    "block_draw": False, "steal_draw": True},
-    }
-}
 
 class Game:
     def __init__(self):
